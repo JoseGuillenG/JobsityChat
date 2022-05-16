a043fade105ed8ce4af221f86c185486e9b97977
Jose Guillen <guillen.jj92@gmail.com>
2022-05-16 01:09:13 -0700

    Added models

--- Bot/Jobsity.Chat.Bot/Jobsity.Chat.Bot.Application/Stock/StockProcessor.cs
+++ Bot/Jobsity.Chat.Bot/Jobsity.Chat.Bot.Application/Stock/StockProcessor.cs
@@ -30,7 +30,19 @@ namespace Jobsity.Chat.Bot.Application.Stock
 
         private string ProcessCsvFile(string csvFileContent)
         {
-            return string.Empty;
+            using (var reader = new StreamReader(@"C:\test.csv"))
+            {
+                List<string> listA = new List<string>();
+                List<string> listB = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    listA.Add(values[0]);
+                    listB.Add(values[1]);
+                }
+            }
         }
     }
 }
