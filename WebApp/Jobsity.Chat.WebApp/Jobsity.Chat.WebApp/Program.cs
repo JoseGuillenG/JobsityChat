21b1ed61e0c8f14ff219038a26f2a4b3bf60bf0b
Jose Guillen <guillen.jj92@gmail.com>
2022-05-17 22:46:27 -0700

    Fixed identity with credentials to make it work

--- WebApp/Jobsity.Chat.WebApp/Jobsity.Chat.WebApp/Program.cs
+++ WebApp/Jobsity.Chat.WebApp/Jobsity.Chat.WebApp/Program.cs
@@ -8,18 +8,29 @@ using Microsoft.Identity.Web.UI;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
+    // Handling SameSite cookie according to https://docs.microsoft.com/en-us/aspnet/core/security/samesite?view=aspnetcore-3.1
+    options.HandleSameSiteCookieCompatibility();
+});
 
+builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration, "AzureAdB2C");
 builder.Services.AddAuthorization(options =>
 {
     // By default, all incoming requests will be authorized according to the default policy.
     options.FallbackPolicy = options.DefaultPolicy;
 });
+
+
 builder.Services.AddRazorPages()
     .AddMicrosoftIdentityUI();
 //builder.Services.AddSignalR();
+
+builder.Services.AddOptions();
+builder.Services.Configure<OpenIdConnectOptions>(builder.Configuration.GetSection("AzureAdB2C"));
 
 var app = builder.Build();
 
@@ -34,6 +45,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+// Add the Microsoft Identity Web cookie policy
+app.UseCookiePolicy();
 app.UseRouting();
 
 app.UseAuthentication();
